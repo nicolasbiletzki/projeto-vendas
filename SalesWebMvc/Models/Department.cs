d6b7e45605b99ed9136e4a051b35301df9e445b7
Nicolas Biletzki <nicolas_biletzki@hotmail.com>
2021-04-05 14:34:31 -0300

    Implementando models - Atributos basicos, associacao e instanciamento.

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
     }
 }
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+    }
+}

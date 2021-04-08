5910ffc4dc9708103560e2652a1a2f0d65966e34
Nicolas Biletzki <nicolas_biletzki@hotmail.com>
2021-04-08 15:12:16 -0300

    Seller Details e Eager Loading

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -31,9 +32,9 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
-
+        
         public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
